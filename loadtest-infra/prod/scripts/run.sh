351cf38c11c5a582e73fe5f0fae1ed6368a8f2fd
0515-Yoonseo-Kim <qnq0615@gmail.com>
2025-03-29 22:59:28 +0900

    feat: 환경변수 설정 수정

--- loadtest-infra/prod/scripts/run.sh
+++ loadtest-infra/prod/scripts/run.sh
@@ -5,6 +5,7 @@ source $(dirname "$0")/env.sh
 APP_NAME="buha-backend"
 IMAGE_URI="${IMAGE_URI}" # GitHub Actions에서 전달됨
 SECRET_NAME="${SSM_SECRET}" # GitHub Actions에서 전달됨
+CODEDEPLOY_BUCKET="${CODEDEPLOY_BUCKET}" # GitHub Actions에서 전달됨
 
 # 시크릿에서 username과 password 꺼내기
 USERNAME=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" \
@@ -15,8 +16,8 @@ PASSWORD_RAW=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" \
 
 PASSWORD=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$PASSWORD_RAW'''))")
 
-# DocumentDB 접속 URI 조립
-MONGO_URI="mongodb://${USERNAME}:${PASSWORD}@docdb-2025-03-28-14-07-29.cluster-cdc4iccm43ba.ap-northeast-2.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+# S3에서 .env.production 다운로드
+aws s3 cp s3://YOUR_BUCKET_NAME/env/.env.production /home/ec2-user/.env.production
 
 # 🔐 ECR 로그인
 aws ecr get-login-password --region ap-northeast-2 \
@@ -29,6 +30,7 @@ docker rm $APP_NAME 2>/dev/null || true
 # 컨테이너 실행
 docker run -d \
   --name $APP_NAME \
-  -e MONGO_URI="$MONGO_URI" \
+  -p 5000:5000 \
+  --env-file /home/ec2-user/.env.production \
   -v /home/ec2-user/global-bundle.pem:/app/global-bundle.pem \
   $IMAGE_URI
