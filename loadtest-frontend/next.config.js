e7d7aa1033637a8a0fbf99baaaacf62e8706e84f
0515-Yoonseo-Kim <qnq0615@gmail.com>
2025-04-30 14:35:43 +0900

    fix: 정적 라우팅 문제 해결을 위한 trailingSlash 설정 추가

--- loadtest-frontend/next.config.js
+++ loadtest-frontend/next.config.js
@@ -1,9 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/register": { page: "/register" },
+      "/login": { page: "/login" },
+      "/profile": { page: "/profile" },
+      "/chat-rooms": { page: "/chat-rooms" },
+      "/chat-rooms/new": { page: "/chat-rooms/new" },
+    };
   },
 };
 
